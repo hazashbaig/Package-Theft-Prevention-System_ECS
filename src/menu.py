e38da787231fbf99c72f84b1a497ce75b3cb77fd
hazashbaig <hazashbaig@gmail.com>
2023-08-12 03:38:36 +0530

    completed menu.py except review option

--- src/menu.py
+++ src/menu.py
@@ -1,40 +1,62 @@
 from face_recognize import face_recognize
 import mysql_python
 import barcode_scanner
+import time
 
 
 if __name__ == '__main__':
     choice = -1
-    while choice != 3:
+    while choice != 4:
         print("\n___Welcome to Package Anti-Theft System  !!___\n")
         print("1) Incoming Package (for delivery personnel)")
-        print("2) Take Package")
-        print("3) Exit System")
+        print("2) Enter into package room to collect package")
+        print("3) Exit out of package room after collecting package")
+        print("4) Exit System")
 
         print("Your choice : ")
         choice = int(input())
     
         if choice == 1:
-            order_no = barcode_scanner.get_barcode()
-            orders = mysql_python.get_student_orders()
+            person = face_recognize()
+            print(person)
 
-            if order_no in orders:
-                print("You can keep the package inside")
-                # Open servo
+            if person == "unknown":
+                print("Keep the package inside")
+                print("servo opened")
+                time.sleep(6)
+                print("servo closed")
             else:
-                print("Package unrecognized")
-        
-        elif choice == 2:
-            # student = face_recognize()
-            # student_orders = mysql_python.get_student_orders(student)
+                print("You are student")
 
-            # Insert barcode scanning function here
-            # print(student)
-            pass
-        
+        elif choice == 2:
+            student = face_recognize()
+            print(student)
+
+            orders = mysql_python.student_orders(student)
+            print(orders)
+
+            if orders:
+                print("servo opened")
+                time.sleep(6)
+                print("servo closed")
+            else:
+                print("You have no orders to take pls go away")
+
         elif choice == 3:
+            student = face_recognize()
+            print(student)
+
+            order_no = barcode_scanner.get_barcode()
+            print(order_no)
+
+            mysql_python.delete_order_for_student(student, order_no)
+
+            print("servo opened")
+            time.sleep(6)
+            print("servo closed")
+
+        elif choice == 4:
             break
-        
         else:
             print("Invalid Choice. Please Try Again...")
         
--- src/mysql_python.py
+++ src/mysql_python.py
@@ -26,7 +26,7 @@ def display_data():
 
 # display_data(sql_conn)
 
-def get_student_orders():
+def get_all_orders():
     global sql_conn
     cursor = sql_conn.cursor()
 
@@ -35,6 +35,7 @@ def get_student_orders():
     cursor.execute(query)
     result = cursor.fetchall()
 
+    orders = []
     # Process JSON data
     for row in result:
         orders_bytes = row[0]  # Assuming the retrieved data is in bytes
@@ -42,10 +43,14 @@ def get_student_orders():
         orders_data = json.loads(orders_str)
 
         order_ids = orders_data.get("Orders", [])
+        
+        for order in order_ids:
+            orders.append(order)
 
-    return orders_ids
 
-def get_student_orders(reg_no):
+    return orders
+
+def student_orders(reg_no):
     global sql_conn
     cursor = sql_conn.cursor()
 
@@ -62,6 +67,36 @@ def get_student_orders(reg_no):
 
         order_ids = orders_data.get("Orders", [])
 
-    return orders_ids
+    return order_ids
     
-    
+def delete_order_for_student(student_reg_no, order_no):
+    global sql_conn
+    cursor = sql_conn.cursor()
+
+    # Fetch data from the "barcode" table for the specified student
+    query = f"SELECT orders FROM barcode WHERE regno = '{student_reg_no}'"
+    cursor.execute(query)
+    result = cursor.fetchone()
+
+    if result:
+        orders_bytes = result[0]  # Assuming the retrieved data is in bytes
+        orders_str = orders_bytes.decode('utf-8')  # Convert bytes to string
+        orders_data = json.loads(orders_str)
+
+        order_ids = orders_data.get("Orders", [])
+
+        if order_no in order_ids:
+            order_ids.remove(order_no)
+            orders_data["Orders"] = order_ids
+
+            # Update the record in the database
+            update_query = f"UPDATE barcode SET orders = '{json.dumps(orders_data)}' WHERE regno = '{student_reg_no}'"
+            cursor.execute(update_query)
+            sql_conn.commit()
+
+            print(f"Order {order_no} removed for student with regno {student_reg_no}")
+        else:
+            print(f"Order {order_no} not found for student with regno {student_reg_no}")
+    else:
+        print(f"Student with regno {student_reg_no} not found.")
+
